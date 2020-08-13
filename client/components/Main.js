bb2f90aed73d46e52b0ceb0c32d2f44042a6c150
Bobby Ton <bobbyt1997@gmail.com>
2020-08-13 14:55:56 -0500

    Add title / style title with shadow

--- client/components/Main.js
+++ client/components/Main.js
@@ -8,6 +8,10 @@ class Main extends React.Component {
   render() {
     return (
       <div className="container">
+        <h1 className="display-1">
+          Karao
+          <small class="text-muted">Q</small>
+        </h1>
         <form>
           <button className="btn btn-dark btn-lg btn-block">Create Room</button>
           <button className="btn btn-dark btn-lg btn-block">Join Room</button>

4c39bff2118dfe165ac465d4602df12c4b07e75c
Bobby Ton <bobbyt1997@gmail.com>
2020-08-15 13:14:44 -0500

    Add form on page upon entry to handle join/create rooms - add styling

--- client/components/Main.js
+++ client/components/Main.js
@@ -10,6 +10,14 @@ class Main extends React.Component {
     return (
       <div>
         <Navbar />
+        <form className="join-createRoom">
+          <div className="formGroup">
+            <label htmlFor="roomName">Room Name</label>
+            <input type="text" name="roomName" className="form-control" id="roomName" placeholder="Enter Room Name" />
+          </div>
+          <button type="submit" className="btn btn-primary">Create Room</button>
+          <button type="submit" className="btn btn-primary">Join Room</button>
+        </form>
       </div>
     )
   }
